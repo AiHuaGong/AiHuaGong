c270f95e2c6e3dafe4060ee81adefcc18dbd9a65
AiHuaGong <mob@ahuagong.com>
2016-06-22 11:12:13 +0800

    添加项目
    添加项目

---
+++ AiHuaGong-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AiHuaGong-C
+//
+//  Created by ap on 16/6/22.
+//  Copyright © 2016年 AHG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
